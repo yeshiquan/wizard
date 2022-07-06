ceea096d25969f54ae32e94c0d7bd8b0949d7f65
Yeshiquan <yeshiquan@gmail.com>
2022-07-06 14:15:57 +0800

    Update and rename main.cpp to feature_extractor.cpp

--- small_test/feature_extractor/feature_extractor.cpp
+++ small_test/feature_extractor/feature_extractor.cpp
@@ -12,23 +12,23 @@ typename ::std::remove_cv<typename ::std::remove_reference<V>::type>::type
 // 引用的情况下得到本体，指针则得到nullptr的type用于默认值
 #define DEPEND_DEFAULT_TYPE(index) \
 typename ::std::remove_cv<typename ::std::conditional< \
-			::std::is_lvalue_reference<D##index>::value, \
-			typename ::std::remove_reference<D##index>::type, \
-			::std::nullptr_t>::type>::type
+            ::std::is_lvalue_reference<D##index>::value, \
+            typename ::std::remove_reference<D##index>::type, \
+            ::std::nullptr_t>::type>::type
 
 // 去除引用或指针得到本体
 #define DEPEND_REAL_TYPE(index) \
 typename ::std::remove_cv<typename ::std::conditional< \
-			::std::is_lvalue_reference<D##index>::value, \
-			typename ::std::remove_reference<D##index>::type, \
-			typename ::std::remove_pointer<D##index>::type>::type>::type
+            ::std::is_lvalue_reference<D##index>::value, \
+            typename ::std::remove_reference<D##index>::type, \
+            typename ::std::remove_pointer<D##index>::type>::type>::type
 
 // 得到依赖类型的指针，T& -> T*，T* -> T**，为了后续可以统一通过*转回
 #define DEPEND_POINTER_TYPE(index) \
 typename ::std::conditional<::std::is_lvalue_reference<D##index>::value, \
-	DEPEND_REAL_TYPE(index)*, \
-	DEPEND_REAL_TYPE(index)**>::type
-	
+    DEPEND_REAL_TYPE(index)*, \
+    DEPEND_REAL_TYPE(index)**>::type
+    
 
 #define DECLARE_DEPENDENCIE(index) \
     const duer::Any* a##index = dependencies.get(index); \
@@ -44,31 +44,31 @@ typename ::std::conditional<::std::is_lvalue_reference<D##index>::value, \
     } \
     DEPEND_POINTER_TYPE(index) pd##index = std::is_lvalue_reference<D##index>::value ? \
             (DEPEND_POINTER_TYPE(index))prd##index :     \
-            (DEPEND_POINTER_TYPE(index))&prd##index ;	
+            (DEPEND_POINTER_TYPE(index))&prd##index ;    
 
 class Feature {
 public:
-	const duer::Any* get() const {
-		return &_value;	
-	}
-	template<typename T>
-	void assign(T&& v) {
+    const duer::Any* get() const {
+        return &_value;    
+    }
+    template<typename T>
+    void assign(T&& v) {
         _value = std::forward<T>(v);
     }
 private:
-	duer::Any _value;
+    duer::Any _value;
 };
 
 class Dependencies {
 public:
-	const duer::Any* get(int32_t index) const {
-		return _features[index]->get();
-	}
-	void add_feature(Feature* feature) {
-		_features.emplace_back(feature);
-	}
+    const duer::Any* get(int32_t index) const {
+        return _features[index]->get();
+    }
+    void add_feature(Feature* feature) {
+        _features.emplace_back(feature);
+    }
 private:
-	std::vector<Feature*> _features;
+    std::vector<Feature*> _features;
 };
 
 
@@ -88,44 +88,46 @@ protected:
 
 template<typename V, typename D0, typename D1>
 class SimpleOperator : public SimpleOperatorBase<
-	VALUE_REAL_TYPE,
+    VALUE_REAL_TYPE,
     DEPEND_DEFAULT_TYPE(0), 
     DEPEND_DEFAULT_TYPE(1)> {
 public:
-	virtual int32_t gen(V& value, const Dependencies& dependencies) const {
-		DECLARE_DEPENDENCIE(0);
-		DECLARE_DEPENDENCIE(1);
-		return generate(value, *pd0, *pd1);
-	}
+    virtual int32_t gen(V& value, const Dependencies& dependencies) const {
+        DECLARE_DEPENDENCIE(0);
+        DECLARE_DEPENDENCIE(1);
+        return generate(value, *pd0, *pd1);
+    }
 protected:
-	virtual int32_t generate(V value, D0 d0, D1 d1) const = 0;
+    virtual int32_t generate(V value, D0 d0, D1 d1) const = 0;
 };
 
 class FoobarOperator : public SimpleOperator<std::string&, const int32_t*, const std::string*> {
-	int32_t generate(std::string& result, const int32_t* age, const std::string* name) const override {
+    int32_t generate(std::string& result, const int32_t* age, const std::string* name) const override {
         std::cout << "age:" << age << " name:" << name << std::endl;
         result = ("age:" + std::to_string(*age) + " name:" + *name);
-	return 0;
-}
-	
+        return 0;
+    }
 };
 
 int main() {
-	int32_t age = 32;
-	std::string name = "kenby";
-	Feature* f1 = new Feature;
-	f1->assign(age);
-	Feature* f2 = new Feature;
-	f2->assign(name);
+    int32_t age = 32;
+    std::string name = "kenby";
+    Feature* f1 = new Feature;
+    f1->assign(age);
+    Feature* f2 = new Feature;
+    f2->assign(name);
 
-	Dependencies dependencies;
-	dependencies.add_feature(f1);
-	dependencies.add_feature(f2);
+    Dependencies dependencies;
+    dependencies.add_feature(f1);
+    dependencies.add_feature(f2);
 
-	std::string result;
-	FoobarOperator* op = new FoobarOperator;
-	op->gen(result, dependencies);
-	std::cout << "result -> " << result << std::endl;
+    std::string result;
+    FoobarOperator* op = new FoobarOperator;
+    op->gen(result, dependencies);
+    std::cout << "result -> " << result << std::endl;
 
-	return 0;
+    duer::Any any = 34;
+    std::cout << *any.get<int32_t>() << std::endl;
+
+    return 0;
 }
