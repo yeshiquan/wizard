e69502050b01e861a5a9ae3290ea2025f8f77084
Yeshiquan <yeshiquan@gmail.com>
2022-07-09 23:56:38 +0800

    Create test_skiplist.cpp

---
+++ src/container/test_skiplist.cpp
@@ -0,0 +1,87 @@
+#include "gtest/gtest.h"
+#include "gflags/gflags.h"
+#include <thread>
+#include <chrono>
+#include <random>
+
+#define  DCHECK_IS_ON
+#define POOL_STATS true
+
+#define private public
+#define protected public
+#include "skiplist/skiplist.h"
+#undef private
+#undef protected
+
+
+class SkipListTest : public ::testing::Test {
+private:
+    virtual void SetUp() {
+        //p_obj.reset(new ExprBuilder);
+    }
+    virtual void TearDown() {
+        //if (p_obj != nullptr) p_obj.reset();
+    }
+protected:
+    //std::shared_ptr<ExprBuilder>  p_obj;
+};
+
+
+// TEST_F(SkipListTest, test_tagged_index) {
+//using folly::ConcurrentSkipList;
+//     typedef ConcurrentSkipList<int> SkipListT;
+//     std::shared_ptr<SkipListT> sl(SkipListT::createInstance(4));   
+//     //SkipListT::Accessor accessor(sl);
+//     sl->addOrGetData(7);
+//     sl->addOrGetData(14);
+//     sl->addOrGetData(21);
+//     sl->addOrGetData(32);
+//     sl->addOrGetData(37);
+//     sl->addOrGetData(71);
+//     sl->addOrGetData(85);
+//     sl->addOrGetData(119);
+//     sl->print();
+//     sl->findNode(85);
+// }
+
+TEST_F(SkipListTest, test_basic) {
+    auto* sl = new rcu::SkipList<int>(4);
+    //auto* sl = new rcu::SkipList<int>(8);
+    std::vector<int> elements = {7, 14, 21, 32, 37, 71, 85, 119};
+
+    // for (int i = 1; i <= 100; ++i) {
+    //     elements.emplace_back(i);
+    // }
+
+    int not_exist_value = 120;
+    bool ret = sl->find(not_exist_value);
+    ASSERT_EQ(ret, false);
+    sl->remove(not_exist_value);
+
+    for (auto e : elements) {
+        bool is_exist = sl->insert(e);
+        ASSERT_EQ(is_exist, false);
+        //插入重复元素
+        is_exist = sl->insert(e);
+        ASSERT_EQ(is_exist, true);
+
+        sl->print();
+        ret = sl->find(e);
+        ASSERT_EQ(ret, true);
+        ret = sl->find(not_exist_value);
+        ASSERT_EQ(ret, false);
+    }
+
+    sl->print();
+
+    for (auto e : elements) {
+        bool ret = sl->find(e);
+        ASSERT_EQ(ret, true);
+
+        sl->remove(e);
+        sl->print();
+        // 删除后就找不到了
+        ret = sl->find(e);
+        ASSERT_EQ(ret, false);
+    }
+}
