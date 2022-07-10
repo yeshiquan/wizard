6281f5f84f98cefb2d3750054765ac258c4aa712
Yeshiquan <yeshiquan@gmail.com>
2022-07-10 13:45:52 +0800

    Update skiplist.h

--- src/container/skiplist.h
+++ src/container/skiplist.h
@@ -22,9 +22,12 @@ public:
     using NodeType = detail::SkipListNode<T> ;
     using ValueType = T;
 
-    SkipList(uint8_t height) : alloc_(NodeAlloc()) {
-        head_ = NodeType::create(alloc_, height, ValueType());
+    SkipList(uint8_t init_height) : alloc_(NodeAlloc()) {
+        head_ = NodeType::create(alloc_, init_height, ValueType());
     }
+    SkipList() : alloc_(NodeAlloc()) {
+        head_ = NodeType::create(alloc_, 8, ValueType());
+    }    
     ~SkipList() {
         NodeType* node = head_;
         while (node) {
@@ -35,150 +38,165 @@ public:
     }
 
     bool insert(const ValueType& data) {
-        int node_height = get_random_height();
-        std::cout<< "insert() node_height:" << node_height << " data:" << data << std::endl;
+        return insert(data, false);
+    }
+
+    bool insert(const ValueType& data, bool is_force_insert) {
+        int current_height = height();
+        size_t newSize = size_ + 1;
+        size_t sizeLimit = detail::SkipListRandomHeight::instance().getSizeLimit(current_height);
+        if (current_height < MAX_HEIGHT && newSize > sizeLimit) {
+            //std::cout << "grow_height() " << sizeLimit << " -> " << newSize << std::endl;
+            grow_height(current_height + 1);
+        }        
+        int node_height = detail::SkipListRandomHeight::instance().getHeight(height());
+        //std::cout<< "insert() node_height:" << node_height << " data:" << data << " list_height:" << height() << std::endl;
         auto* new_node = NodeType::create(alloc_, node_height, data);
-        bool is_exist = find_insert_point(new_node, data);
-        if (is_exist) {
-            NodeType::destroy(alloc_, new_node);
+
+        std::vector<NodeType*> prevs(height());
+        auto* next_node = _search_equal_or_greater(data, prevs);
+        bool is_exist = next_node && next_node->data() == data;
+
+        if ((is_exist && is_force_insert) || !is_exist) {
+            for (int layer = node_height - 1; layer >= 0; layer--) {
+                new_node->set_skip(layer, prevs[layer]->skip(layer));
+                prevs[layer]->set_skip(layer, new_node);
+            }
+            size_++;
         }
         return is_exist;
     }
 
-    void remove(const ValueType& data) {
-        std::cout << "remove(" << data << ")" << std::endl;
+    bool remove(const ValueType& data) {
+        //std::cout << "remove(" << data << ")" << std::endl;
         int layer = height() - 1;
         NodeType* node = head_;
         NodeType* node_next = nullptr;
-        NodeType* to_delete_node = nullptr;
-        while (layer >= 0) {
-            // 双指针：寻找 node < data <= node_next 这样的点
-            node_next = node->skip(layer);
-            while (node_next && greater(data, node_next->data())) {
-                node = node_next;
-                node_next = node_next->skip(layer);
-            }
-            if (node_next && equal(data, node_next->data())) {
-                to_delete_node = node_next;
-                node->set_skip(layer, node_next->skip(layer));
-            }
-            layer--;
+
+        std::vector<NodeType*> prevs(height());
+        auto* to_delete_node = _search_equal_or_greater(data, prevs);
+
+        if (to_delete_node == nullptr || to_delete_node->data() != data) {
+            return false;  
         }
-        if (to_delete_node != nullptr) {
-            NodeType::destroy(alloc_, to_delete_node);
+
+        for (int l = to_delete_node->height() - 1; l >= 0; --l) {
+            prevs[l]->set_skip(l, to_delete_node->skip(l));
         }
+
+        NodeType::destroy(alloc_, to_delete_node);
+        size_--;
+        return true;
     }
 
-    bool find_insert_point(NodeType* new_node, const ValueType& data) {
-        int layer = new_node->height() - 1;
-        NodeType* node = head_;
-        NodeType* node_next = nullptr;
-        std::vector<std::tuple<NodeType*, NodeType*, int>> items;
-        items.reserve(layer+1);
-        bool is_exist = false;
-        while (layer >= 0) {
-            // 双指针：寻找 node < data <= node_next 这样的点
-            node_next = node->skip(layer);
-            while (node_next && greater(data, node_next->data())) {
-                node = node_next;
-                node_next = node_next->skip(layer);
-            }
-            if (node_next && equal(data, node_next->data())) {
-                is_exist = true;
-                break;
-            }
-            items.emplace_back(std::make_tuple(node, node_next, layer));
-            layer--;
+    void grow_height(int new_height) {
+        if (head_->height() >= new_height) {
+            // 别人做了grow
+            return;
         }
-
-        if (!is_exist) {
-            for (auto& item : items) {
-                node = std::get<0>(item);
-                node_next = std::get<1>(item);
-                layer = std::get<2>(item);
-                node->set_skip(layer, new_node);
-                new_node->set_skip(layer, node_next);
-            }
-        }
-
-        return is_exist;
-    }    
+        NodeType* old_head = head_;
+        NodeType* new_head = NodeType::create(alloc_, new_height, ValueType());
+        new_head->copy_skip_pointer(old_head);
+        head_ = new_head;
+        NodeType::destroy(alloc_, old_head);
+    }
 
     bool find(const ValueType& data) {
-        return _find_node_down_right(data);
+        auto* node = _search_equal_or_greater(data);
+        return node != nullptr && node->data() == data;        
     }
 
-  void print_layer(NodeType* start, int layer) {
+  void print_layer(NodeType* start, int layer, int& n) {
       NodeType* node = start;
       std::cout << "layer[" << layer << "] -> ";
       while (node) {
           std::cout << node->data() << ",";
+          n++;
           node = node->skip(layer);
       }
       std::cout << std::endl;
   }
 
   void print() {
-      printf("\n============ SkipList print begin ================\n");
+      printf("\n============ SkipList print begin(%d) ================\n", size_);
+      int n = 0;
       auto top = maxLayer();
       for (int layer = top; layer >= 0; --layer) {
-          print_layer(head_->skip(layer), layer);
+          n = 0;
+          print_layer(head_->skip(layer), layer, n);
       }
-      printf("============ SkipList print end ================\n\n");
+      printf("============ SkipList print end(%d) ================\n\n", n);
+      if (size_ != n) {
+          printf("ERROR elements size invalid\n");
+      }
   }
-    int int_rand(const int & min, const int & max) {
-        static thread_local std::mt19937 generator;
-        std::uniform_int_distribution<int> distribution(min,max);
-        return distribution(generator);
-    }
-    int get_random_height() {
-        int K = 1;
-        while (int_rand(0, 1) && K < height()) {
-            K++;
-        }
-        return K;
-    }
   int height() const { return head_->height(); }
   int maxLayer() const { return height() - 1; }  
+  int size() const { return size_; }  
 private:
-    bool _find_node_down_right(const ValueType& data) {
-        std::cout << "find(" << data << ")" << std::endl;
-        NodeType* node = head_;
-        int layer = head_->height() - 1;
-        NodeType* node_next = nullptr;
-
-        bool found = false;
-        while (true) {
-            // 双指针：寻找 node < node_next <= data 的节点
-            while (layer >= 0) {
-                node_next = node->skip(layer);
-                if (node_next == nullptr || greater(node_next->data(), data)) {
-                    // 跳过 node_next > data的层, node_next==nullptr当做无穷大来处理
-                    layer--;
-                } else {
-                    break;
-                }
+    // 寻找 node >= data的节点，同时返回前驱节点
+    NodeType* _search_equal_or_greater(const ValueType& data, 
+                                            std::vector<NodeType*>& prevs) {
+        NodeType* cur = head_;
+        NodeType* next = nullptr;
+        for(int i = maxLayer(); i >= 0; i--){
+            next = cur->skip(i);
+            while(next && less(next->data(), data)){
+                // 一直寻找到刚好大于等于data的节点的前置节点
+                cur = next;
+                next = next->skip(i);
             }
+            prevs[i] = cur;
+        }
+        return next;       
+    }
 
-            //std::cout << "find() layer:" << layer << " node_next:" << node_next << std::endl;
-            if (layer < 0) {
-                found = false;
-                break;
-            }
+    // // 容易导致性能恶化的实现
+    // bool find_insert_point(NodeType* new_node, const ValueType& data) {
+    //     NodeType* node = head_;
+    //     NodeType* node_next = nullptr;
+    //     std::vector<NodeType*> prevs(new_node->height());
 
-            // 双指针：寻找 node < data <= node_next 的节点
-            while (node_next && less(node_next->data(), data)) {
-                node = node_next;
-                node_next = node_next->skip(layer);
-            }
-            if (node_next != nullptr && equal(node_next->data(), data)) {
-                found = true;
-                break;
+    //     int layer = new_node->height() - 1;  // Bad
+    //     int layer = maxLayer();              // Good
+    //     // layer如果从新节点开始初始化，会导致性能恶化，
+    //     // 因为新节点的高度可能很小，这样会从中间某一层节点从头开始搜索，中间这层节点数很多的时候，性能恶化
+    //     // 永远要从最顶层开始搜索，才能充分利用跳表的索引
+    //     // 永远不要从中间或者最底层节点搜索，性能会退化很严重
+    //     for (; layer >= 0; --layer) {
+    //         node_next = node->skip(layer);
+    //         while (node_next && (data > node_next->data())) {
+    //             node = node_next;
+    //             node_next = node_next->skip(layer);
+    //         }
+    //         prevs[layer] = node;
+    //     }
+    //     for (int layer = new_node->height() - 1; layer >= 0; layer--) {
+    //         new_node->set_skip(layer, prevs[layer]->skip(layer));
+    //         prevs[layer]->set_skip(layer, new_node);
+    //     }        
+    //     bool is_exist = new_node->skip(0) && new_node->skip(0)->data() == data;
+ 
+    //     size_++;  
+    //     return is_exist;
+    // }    
+
+
+    // 寻找 node >= data 的节点
+    NodeType* _search_equal_or_greater(const ValueType& data) {
+        NodeType* cur = head_;
+        NodeType* next = nullptr;
+        for(int i = maxLayer(); i >= 0; i--){
+            next = cur->skip(i);
+            while(next && less(next->data(), data)){
+                // 一直寻找到刚好大于等于data的节点的前置节点
+                cur = next;
+                next = next->skip(i);
             }
         }
-        return found;
-    }
-    
+        return next;
+    }        
+
   static bool greater(const ValueType &data1, const ValueType& data2) {
     return Comp()(data2, data1);
   }
